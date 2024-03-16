ba25da0851935a5e879a72902433a24799cb19be
Priyanshu <sahaashutosh108@gmail.com>
2024-03-16 09:00:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,37 +13,33 @@ var feecount2;
 var num
 var count;
 
-plusbutn.addEventListener('click', () => 
-{
+plusbutn.addEventListener('click', () => {
 
-        num = parseInt(tex.innerText);
-        count = num + 1;
-        tex.innerText = count;
-        feenum = parseInt(fee.innerText);
-        feecount = feenum + 600;
-        fee.innerText = feecount;
-        feenum2 = parseInt(fee2.innerText);
-        feecount2 = feenum2 + numbo + 600 - 52;
-        fee2.innerText = feecount2;
-        console.log("count = ", count);
+    num = parseInt(tex.innerText);
+    count = num + 1;
+    tex.innerText = count;
+    feenum = parseInt(fee.innerText);
+    feecount = feenum + 600;
+    fee.innerText = feecount;
+    feenum2 = parseInt(fee2.innerText);
+    feecount2 = feenum2 + numbo + 600 - 52;
+    fee2.innerText = feecount2;
+    console.log("count = ", count);
 })
 minusbutn.addEventListener('click', () => {
-    
-    if(count <= 1)
-    {
+
+    if (count <= 1) {
         document.getElementById('butni').style.display = 'none';
         document.getElementById('carty').style.display = 'none';
         document.getElementById('cancel').style.display = 'flex';
     }
-    if(count == 1)
-    {
+    if (count == 1) {
         const v = 652
         fee2.innerText = v;
     }
 
-   
-    else
-    {
+
+    else {
 
         num = parseInt(tex.innerText);
         count = num - 1;
@@ -59,3 +55,11 @@ minusbutn.addEventListener('click', () => {
     }
 })
 
+
+let book = document.getElementById('book');
+
+book.addEventListener('click', () => {
+    document.getElementById('succs').style.display = 'block';
+    document.getElementById('butni').style.display = 'none';
+    document.getElementById('carty').style.display = 'none';
+})
